4f2900bd79edaa5660ca4f5ce5e98fa5160e1a24
Sarath Babu <113832685+esarath@users.noreply.github.com>
2022-11-25 14:02:51 +0530

    Update backend.tf

--- 03_AKS_azure-kubernetes/backend.tf
+++ 03_AKS_azure-kubernetes/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "cloud-shell-storage-centralindia"
     storage_account_name = "csg100320024380adcb"
-    container_name       = "tfstate"
+    container_name       = "tfstate-aks"
     key                  = "terraform.tfstate"
   }
 }
